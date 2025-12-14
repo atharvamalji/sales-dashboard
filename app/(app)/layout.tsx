2a031ed0649508106e90d76ca5942773738c9042
Atharva Malji <atharva.malji.2001@gmail.com>
2025-12-14 15:59:00 +0530

    updated site name

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -4,7 +4,7 @@ import "../globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Salescast.io",
+  title: "SuperStore Insights",
   description: "Sales management and visualization platform",
 };
 
